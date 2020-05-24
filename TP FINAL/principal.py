6adaefb7c79e4253b3d41f9cdd89e52fe5046747
manurua123 <62101533+manurua123@users.noreply.github.com>
2020-05-24 14:20:00 -0300

    Add files via upload

--- TP FINAL/principal.py
+++ TP FINAL/principal.py
@@ -1,13 +1,13 @@
 import configuracion
 import PySimpleGUI as sg
 layout = [
-    [sg.Text('ScrabbleAR')],
-    [sg.Button('Jugar',)],
-    [sg.Button('Configurar')],
-    [sg.Button('Salir')]
+    [sg.Text('ScrabbleAR',size= (20,2),font = ('Arial', 12, 'bold'),)],
+    [sg.Button('Jugar',size= (20,2))],
+    [sg.Button('Configurar',size= (20,2))],
+    [sg.Button('Salir',size= (20,2))]
 ]
 
-window = sg.Window('', layout)
+window = sg.Window('', layout, text_justification='center',size= (200,200),)
 while True:
     event, value = window.read()
     if event is None or event == 'Salir':
